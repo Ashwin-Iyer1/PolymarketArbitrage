82469f183e7a1acc43707cb5a2745367f1132072
Ashwin-Iyer1 <ashwiniyer06@gmail.com>
2024-12-05 15:46:21 +0000

    Refactor data handling and add new functions for market analysis

--- start.py
+++ start.py
@@ -4,6 +4,7 @@ import os
 import json
 import base64
 import datetime
+import itertools 
 
 load_dotenv()
 
@@ -17,7 +18,12 @@ creds = client.create_or_derive_api_creds()
 
 client.set_api_creds(creds)
 
-def writeData():
+def writeData(data, name):
+    with open(f"{name}.json", "w") as f:
+      json.dump(data, f)
+
+
+def getData(tag: str = None):
     today = datetime.datetime.now()
     #save to json
     num = 20000
@@ -29,20 +35,84 @@ def writeData():
             if(market["end_date_iso"] != None):
                 if(today < datetime.datetime.fromisoformat(market["end_date_iso"][:-1])):
                     if(market["is_50_50_outcome"] == False):
-                        listofMarket.append(market)
+                        if(market["closed"] != True and market["active"] == True):
+                            if tag != None:
+                                if tag in market["tags"]:
+                                    listofMarket.append(market)
+                            else:
+                                listofMarket.append(market)
         if markets["next_cursor"] == "LTE=":
             break
         cursor = markets["next_cursor"]
-    with open(f"markets.json", "w") as f:
-      json.dump(listofMarket, f)
+    return listofMarket
 
 
 
-client.get_order_book("46233341984141805339503017327792472865621457040314248924266388854799239420097")
+
+
+def parse_data(range: int, MarketsList: list, DTEMax: int, DTEMin: int):
+    parsed_data = []
+
+    for market in MarketsList:
+        enddate = datetime.datetime.fromisoformat(market["end_date_iso"][:-1])
+        startdate = datetime.datetime.fromisoformat(market["accepting_order_timestamp"][:-1])
+        DTEcalc = datetime.datetime.fromisoformat(market["end_date_iso"][:-1]) - datetime.datetime.now()
+        if market["end_date_iso"] != None:
+            if(enddate - startdate > datetime.timedelta(days=range)):
+                parsed_data.append(market)
+    return parsed_data
 
 
 
-        
+def getMarket(market):
+    return client.get_market(market)
+
+def getTokenIDS(market):
+    tokens = []
+    for token in market["tokens"]:
+        tokens.append(token["token_id"])
+    return tokens
+
+def getOrderBook(token):
+    result = client.get_order_book(token)
+    return result
+
+def getPopularity(OrderBook):
+    OrderBook.json()
+
+
+with open("ParsedPoliticsMarkets.json", "r") as f:
+    data = json.load(f)
+
+
+def main():
+    marketsSorted = {}
+    for market in data:
+        sample_data = getTokenIDS(market)
+        for token in sample_data:
+            new_data = getOrderBook(token)
+            total = 0
+            for order in new_data.bids:
+                total += float(order.size)
+            for order in new_data.asks:
+                total += float(order.size)
+            marketsSorted[market["condition_id"]] = total
+
+    sortedDict = dict(sorted(marketsSorted.items(), key=lambda item: item[1], reverse=True))
+    top5 = dict(list(sortedDict.items())[0: 5])
+    for key in top5:
+        print(key, top5[key])
+        print("buy spread at " + str(getMarket(key)["tokens"][0]["price"]))
+
+main()
+# sample_data = getData("Politics")
+
+
+
+
+# new_data = parse_data(14, sample_data, 5, 2)
+
+# writeData(new_data, "ParsedPoliticsMarkets")
 
 
 
